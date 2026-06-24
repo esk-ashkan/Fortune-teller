cdd628eb45c0ff353798bead41cf8951a8ab4799
AshkanEsk <eskandary.ashkan@gmail.com>
2026-06-24 09:14:38 +0330

    Fix port binding and add gunicorn

--- Back-end/app.py
+++ Back-end/app.py
@@ -16,7 +16,9 @@ def home():
 def health(): 
     return {"status": "ok"} 
  
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=port)
+
 
