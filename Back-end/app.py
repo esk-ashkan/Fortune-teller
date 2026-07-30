85ea6fd0438ec8668b6f3f33b7afbde785b5eea6
AshkanEsk <eskandary.ashkan@gmail.com>
2026-07-30 10:41:14 +0330

    Stablish New Models

--- Back-end/app.py
+++ Back-end/app.py
@@ -8,7 +8,6 @@ from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from google.genai import types
-from mistralai.client import Mistral
 import requests
 
 # --------------------------------------------------
